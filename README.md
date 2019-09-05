debaa334505f7ee93f6bf6ad0c826e7748a3c2ea
Greg Trost <gltrost2@gmail.com>
2019-09-05 14:08:30 -0700

    some documentation added

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 This project began in December, 2018 and is still on-going. 
 It will become an interactive crossword site where multiple people may fill in a crossword simultaneously from different computers. 
 
-Some important files: 
+Important files: 
   applications.py : The main python application folder, linked to puz.py
   puz.py : Crossword parser, taken from alexdef/puzpy, found here https://github.com/alexdej/puzpy/blob/master/puz.py
   CW.html : main html page
--- application.py
+++ application.py
@@ -8,68 +8,33 @@
 # p = puz.read('testfiles/washpost.puz')
 
 
-
-
-# numbering = p.clue_numbering()
-
-# print('Across')
-# for clue in numbering.across:
-#     answer = ''.join(
-#         p.solution[clue['cell'] + i]
-#         for i in range(clue['len']))
-#     print( clue['num'], clue['clue'], '-', answer)
-
-# print( 'Down')
-# for clue in numbering.down:
-#     answer = ''.join(
-#         p.solution[clue['cell'] + i * numbering.width]
-#         for i in range(clue['len']))
-#     print( clue['num'], clue['clue'], '-', answer)
-
-# for row in range(p.height):
-#     cell = row * p.width
-#     # Substitute p.solution for p.fill to print( the answers
-#     print( ' '.join(p.fill[cell:cell + p.width]))
-
-# p.unlock_solution(7844)
-# # p.solution is unscambled
-
-# p.fill = 'LAMB' + p.fill[4:]
-# p.save('mine.puz')
-
 from flask import Flask, request, render_template
 import sys
 import puz
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-# class Crossword :
-# 	def __init__(self,title,height,width,across,down,solution):
-# 		self.title = title
-# 		self.height = height
-# 		self.width = width
-# 		self.across = across
-# 		self.down = down
-# 		self.solution = solution	
-	
-# 	def size(self):
-# 		return self.width * self.height
-
 
 p = puz.read('testfiles/washpost.puz')
 numbering = p.clue_numbering()
 
-# These are the lists of clues, with each clue formatted as follows:
-# {'num': 1, 'clue': "Mary's pet", 'cell': 0, 'len': 4
-# cw = Crossword(title,height,width,across,down,solution)
-
 app = Flask(__name__)
+
+# METHODS CALLED FROM puz.py :
+#    title: title of crossword
+#    across: across clues
+#    down: down clues
+#    Height: height of board
+#    Width: width of board
+#    solutions: list of solutions
+#    author: creator of puzzle
+#    copyright: any copyright information
 
 @app.route('/')
 def index():
    return render_template('CW.html', title=p.title ,marks = 10, 
-   							across=numbering.across, 
-   							down=numbering.down, 
+   							across= numbering.across, 
+   							down= numbering.down, 
    							Height = p.height, 
    							Width = p.width,
    							listSize = p.height * p.width,
