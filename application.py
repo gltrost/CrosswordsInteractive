24d597a00face0da17aa20c3292c8ed9ec4b5b8a
Greg Trost <gltrost2@gmail.com>
2019-09-14 15:12:13 -0600

    highlighting-feature now in progress

--- application.py
+++ application.py
@@ -1,14 +1,8 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-
-# import puz
-# import sys
-# p = puz.read('testfiles/washpost.puz')
-
-
 from flask import Flask, request, render_template
+from flask_scss import Scss
 import sys
 import puz
 reload(sys)
@@ -19,6 +13,17 @@ p = puz.read('testfiles/15by15.puz')
 numbering = p.clue_numbering()
 
 app = Flask(__name__)
+Scss(app, static_dir='static', asset_dir='assets')
+
+# adjuster : 
+# takes in a crossword width and returns a value to 
+# adjust the size of the board, allowing for large, medium and small 
+# puzzles to all occupy approximately the same size on the screen 
+def adjuster(x):
+   if (x > 20): return 1.5
+   elif (x > 10): return 2.5
+   else: return 3.5
+
 
 # METHODS CALLED FROM puz.py :
 #    title: title of crossword
@@ -27,15 +32,9 @@ app = Flask(__name__)
 #    Height: height of board
 #    Width: width of board
 #    solutions: list of solutions
+#    lcSolutions : solutions in all lower-case (needed for pattern matching right answers)
 #    author: creator of puzzle
 #    copyright: any copyright information
-
-def adjuster(x):
-   if (x > 20): return 1.5
-   elif (x > 10): return 2.5
-   else: return 3.5
-
-
 @app.route('/')
 def index():
    return render_template('CW.html', title=p.title ,marks = 10, 
@@ -45,6 +44,7 @@ def index():
    							Width = p.width,
    							listSize = p.height * p.width,
    							solutions = p.solution,
+                        lcSolutions = p.solution.lower(),
    							author = p.author,
    							copyright = p.copyright,
                         adjust = adjuster(p.width))	
